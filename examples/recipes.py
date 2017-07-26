e7934e2bb8bba74b54623dc50a2914c4b57f3c39
Bas Rustenburg <bas-rustenburg@users.noreply.github.com>
2017-07-26 12:40:18 -0400

    Update recipes.py

--- examples/recipes.py
+++ examples/recipes.py
@@ -8,13 +8,18 @@ CAII_ITC_Buffer = buffr.Sodium_Phosphate_buffer
 volume = buffr.u.Quantity(1, 'liter')
 concentration = buffr.u.Quantity(20, 'millimole/liter')
 
-
 print("Kinase Assay Buffer acid/base component recipe for pH")
-for pH in np.linspace(7.0,9.0,21):
+for pH in np.linspace(7.0, 9.0, 21):
     print(Kinase_Binding_Assay_Buffer.recipe_at_ph(pH, concentration, volume))
 
-
-print("CB7 Host Guest acid/base acid/base component recipe for pH")
-for pH in np.linspace(6.2,8.2,21):
+print("CAII buffer acid/base component recipe for pH")
+for pH in np.linspace(6.2, 8.2, 21):
     print(CB7_Host_Guest_Buffer.recipe_at_ph(pH, concentration, volume))
 
+volume = buffr.u.Quantity(1, 'liter')
+concentration = buffr.u.Quantity(100, 'millimole/liter')
+
+print("CB7 Host Guest acid/base component recipe for pH")
+for pH in np.linspace(6.2, 8.2, 21):
+    print(CB7_Host_Guest_Buffer.recipe_at_ph(pH, concentration, volume))
+    
