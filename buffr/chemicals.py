6761a9cb06a2a81706da648416102d9caf9b223e
Bas Rustenburg <bas.rustenburg@choderalab.org>
2017-07-26 13:20:17 -0400

    Fix sodium buffer phosphate bugs, and add sodium chloride.

--- buffr/chemicals.py
+++ buffr/chemicals.py
@@ -30,3 +30,4 @@ TRIS_base = DryChemical('TRIS (Base), Ultrapure Bioreagent', Q(121.14, 'gram/mol
 Trizma_hydrochloride = DryChemical('Trizma Hydrochloride Reagent Grade 99.0%', Q(157.6, 'gram/mole'), 99.0)
 Sodium_Phosphate_Monobasic_Dihydrate = DryChemical('Sodium Phosphate Monobasic Dihydrate BioUltra', Q(156.01, 'gram/mole'), 99.0)
 Sodium_Phosphate_Dibasic = DryChemical('Sodium Phosphate Dibasic Puriss', Q(141.96, 'gram/mol'), 99.0)
+Sodium_Chloride = DryChemical('Sodium Chloride ReagentPlus 99.0%', Q(58.44, 'gram/mole'), 99.0)
